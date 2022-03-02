e1acd82c13e9a2ca9c70f7c05733f0378c905f78
Keivan Tafakkori <91466697+Keivan-Tafakkori@users.noreply.github.com>
2022-03-02 02:36:27 -0800

    Update drake_optimize.py

--- drake_optimize.py
+++ drake_optimize.py
@@ -20,7 +20,7 @@ prob.AddConstraint(5*(x[0]+2*x[1]+4*x[2]+6*x[3])+3*y[0]<=10)
 prob.AddConstraint(2*(x[0]+2*x[1]+4*x[2]+6*x[3])+7*y[0]<=9)
 
 # The status of the solution
-result = GurobiSolver().Solve(prog)
+result = GurobiSolver().Solve(prob)
 print(result.is_success())
 
 # To display optimal decision variables

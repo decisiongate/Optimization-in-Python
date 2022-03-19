bb4d6b99cdbc7e9d9839f6107bb4a09e86543191
Keivan Tafakkori <91466697+Keivan-Tafakkori@users.noreply.github.com>
2022-03-19 22:20:33 +0330

    Update install_upgrade_interfaces.py

--- install_upgrade_interfaces.py
+++ install_upgrade_interfaces.py
@@ -4,12 +4,13 @@
 !pip install cvxopt
 !pip install cvxpy
 !pip install pymprog
-!pip install drake
 !pip install ortools
 !pip install gekko
 !pip install pymoo
 !pip install mip
 !pip install cylp
+#Does not work with Anaconda:
+!pip install drake 
 
 #To upgrade 
 !pip install --upgrade pyomo
@@ -17,9 +18,10 @@
 !pip install --upgrade cvxopt
 !pip install --upgrade cvxpy
 !pip install --upgrade pymprog
-!pip install --upgrade drake
 !pip install --upgrade ortools
 !pip install --upgrade gekko
 !pip install --upgrade pymoo
 !pip install --upgrade mip
 !pip install --upgrade cylp
+#Does not work with Anaconda:
+!pip install --upgrade drake

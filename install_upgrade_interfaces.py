78b8c33b1459e5de48fd1019c4266aed2f11bcac
Keivan Tafakkori <91466697+Keivan-Tafakkori@users.noreply.github.com>
2022-03-19 22:15:13 +0330

    Create install_upgrade_interfaces.py

---
+++ install_upgrade_interfaces.py
@@ -0,0 +1,27 @@
+#To install 
+
+!pip install pyomo
+!pip install pulp
+!pip install cvxopt
+!pip install cvxpy
+!pip install pymprog
+!pip install drake
+!pip install ortools
+!pip install gekko
+1pip install pymoo
+!pip install mip
+!pip install cylp
+
+
+#To upgrade 
+!pip install --upgrade pyomo
+!pip install --upgrade pulp
+!pip install --upgrade cvxopt
+!pip install --upgrade cvxpy
+!pip install --upgrade pymprog
+!pip install --upgrade drake
+!pip install --upgrade ortools
+!pip install --upgrade gekko
+1pip install --upgrade pymoo
+!pip install --upgrade mip
+!pip install --upgrade cylp
